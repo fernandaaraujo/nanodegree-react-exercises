e36ba3d2a04c6463396dc85190b26a8470ff2d3b
Fernanda Araujo <nanda.arf@gmail.com>
2017-09-15 12:29:32 -0300

    Populates list contacts component

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class ListContacts extends Component {
   render() {
     return (
       <ol className="contact-list">
+        {this.props.contacts.map(contact => (
+          <li key={contact.id}>
+            {contact.name}
+          </li>
+        ))}
       </ol>
-    )
+    );
   }
 }
+
+ListContacts.propTypes = {
+  contacts: PropTypes.array
+};
 
 export default ListContacts;
